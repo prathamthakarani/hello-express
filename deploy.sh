f87bfdd5bb287e7f0e3d402ac2586192ce22f3e8
pratham-thakarani <thakranipratham@gmail.com>
2024-08-23 12:28:27 +0530

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
-docker build -t hello-express . --no-cache &&
+#!/bin/bash
+
+git pull
+
+# Build the Docker image without cache
+docker build -t hello-express . --no-cache
+
+# Stop all running containers
+docker stop $(docker ps -q)
+
+# Run the new container
 docker run -p 80:3001 --restart=always -d hello-express
